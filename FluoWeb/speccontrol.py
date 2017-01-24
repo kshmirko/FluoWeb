f43e4bc83b600fd1e3dc0b27b5ff697579e6a722
kshmirko <shmirko.konstantin@gmail.com>
2017-01-24 11:13:43 +1000

    change speccontrol.py

--- FluoWeb/speccontrol.py
+++ FluoWeb/speccontrol.py
@@ -1,7 +1,7 @@
 import serial
 
 def move_spec(device, wl):
-    cmd = ("%.3f goto\r"%wl).encode('utf-8')
+    cmd = ("%.1f goto\r"%wl).encode('utf-8')
     print('SPECTRO: %s'%cmd)
     dev = serial.Serial(device, timeout=600)
     dev.write(cmd)
